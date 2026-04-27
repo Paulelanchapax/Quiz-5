6d42c8d36197341842b890cfc45b39afcd940a99
Paulelanchapax <2005paulalejandro@gmail.com>
2026-04-27 02:59:27 -0500

    Aniado el bloque 0 a mi cpp con los comentarios y todo incluido

---
+++ rpg.cpp
@@ -0,0 +1,52 @@
+/*
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct Espada {
+string nombre; // public por defecto
+int dano;
+}; // <-- quita este ';' y mira el error del compilador
+int main() {
+Espada e;
+e.nombre = "Hacha";
+e.dano = 42;
+cout << e.nombre << " -- dano: " << e.dano << endl;
+return 0;
+}
+
+*/
+
+//Anotaciones de comprension del programa mias (que no se piden pero las pongo)
+
+//creamos un struct que es espada, que sirve para agrupar datos en una sola unidad, pero todo es publico por defecto, segun lei
+// entonces creamos string nombre e int de danio, si quitamos el ; supongo que se ha de bugear, luego despues en el main
+//creamos una espadita llamada e porque nuestra "clase" o "struct" es como q la cosa del objeto real echa conjunto, osea
+//son las espadas luego ponemos e.nombre (nombre de nuestra categoria espada y objeto e), luego e danio, que usamos la misma e 
+//que es la categoria espada e y le damos el danio, luego escribimos todo en la pantalla
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct Espada {
+string nombre; // public por defecto
+int dano;
+} // <-- quita este ';' y mira el error del compilador
+int main() {
+Espada e;
+e.nombre = "Hacha";
+e.dano = 42;
+cout << e.nombre << " -- dano: " << e.dano << endl;
+return 0;
+}
+
+//quitamos el ";" y el programa no corre, no funciona
+
+//CHEKPOINTTT!
+
+//El programa si ejecuta el nombre de la espada "Hacha", y su danio "42"
+//Vi el error que daba cuando se quitaba el ";" de la clase (struct)
+//Entendi la diferencia entre struct y class, que una es publica y la otra privada por defecto de ambas.
+//Entendi lo que son private y public en la clase, pero aun nose a que se refiere protected. Talvez lo vea mas adelante
+
